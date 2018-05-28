572b1f41bb6bb1c27b6a2a6ac0643f4c4f5dad5f
Internet Of Mine <internetofmine@gmail.com>
2018-05-28 21:08:45 +0200

    Adding simple logic to Menu Scene
    https://app.asana.com/0/687819990564529/687819990564531/f

---
+++ SimpleShooter/Assets/Scripts/Managers/MenuManager/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager: MonoBehaviour
+{
+
+    public void NavigateToGame()
+    {
+        SceneManager.LoadScene("CombatScene", LoadSceneMode.Single);
+    }
+
+    public void NavigateToExit()
+    {
+        Application.Quit();
+    }
+
+}
