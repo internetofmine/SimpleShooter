1d3cd9687ed39f75ff1e009bc690b47f4077394b
Internet Of Mine <internetofmine@gmail.com>
2018-05-31 22:53:55 +0200

    Simple string storage for translations
    https://app.asana.com/0/687819990564529/693638237579770/f

---
+++ SimpleShooter/Assets/Scripts/Translations/Lang.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lang
+{
+    public string CombatScene_QuitTitle = "Confirm Exit";
+    public string CombatScene_QuitMessage = "Your application will exit now";
+    public string CombatScene_QuitConfirm = "OK";
+    public string CombatScene_QuitCancel = "Cancel";
+}
---
+++ SimpleShooter/Assets/Scripts/Translations/Lang_PL.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lang_PL:Lang
+{
+    public new string CombatScene_QuitTitle = "Potwierdź wyjście";
+    public new string CombatScene_QuitMessage = "Aplikacja się wyłączy";
+    public new string CombatScene_QuitConfirm = "OK";
+    public new string CombatScene_QuitCancel = "Anuluj";
+}
