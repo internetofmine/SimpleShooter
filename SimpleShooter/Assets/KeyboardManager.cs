3ecc2e0f5cc23ee1376ee7f1c2c7196fcd7a9bdc
Internet Of Mine <internetofmine@gmail.com>
2018-05-31 22:54:32 +0200

    Simple Keyboard Manager
    https://app.asana.com/0/687819990564529/693638237579771/f

---
+++ SimpleShooter/Assets/KeyboardManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class KeyboardManager : MonoBehaviour
+{
+    public CombatSceneNavigationManager combatSceneNavigationManager;
+
+    Lang languageUsed = new Lang();
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitDialog();
+        }
+	}
+
+    public void QuitDialog()
+    {
+        if (EditorUtility.DisplayDialog(languageUsed.CombatScene_QuitTitle, languageUsed.CombatScene_QuitConfirm, languageUsed.CombatScene_QuitConfirm, languageUsed.CombatScene_QuitCancel))
+        {
+            combatSceneNavigationManager.NavigateToMenu();
+        };
+    }
+}
