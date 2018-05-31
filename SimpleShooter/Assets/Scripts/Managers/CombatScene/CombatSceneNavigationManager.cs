7e8f07d7cc40c9cd8ec114f21057c161e9999bc5
Internet Of Mine <internetofmine@gmail.com>
2018-05-31 22:55:19 +0200

    Option to navigate to Menu
    https://app.asana.com/0/687819990564529/688710349149090/f

---
+++ SimpleShooter/Assets/Scripts/Managers/CombatScene/CombatSceneNavigationManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CombatSceneNavigationManager : MonoBehaviour {
+
+    public void NavigateToMenu()
+    {
+        SceneManager.LoadScene("MenuScene", LoadSceneMode.Single);
+    }
+}
