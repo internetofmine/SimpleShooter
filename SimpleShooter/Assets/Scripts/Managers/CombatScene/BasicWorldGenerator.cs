4745985b00d60bcc9e5165261d697c03e2742f7d
Internet Of Mine <internetofmine@gmail.com>
2018-06-10 21:06:05 +0200

    [WIP] Basic World Generator
    Slowly building a stub for a world generator.
    https://app.asana.com/0/236703775280313/687819990564533

---
+++ SimpleShooter/Assets/Scripts/Managers/CombatScene/BasicWorldGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicWorldGenerator : MonoBehaviour {
+
+    bool initialized = false;
+
+    LevelLayout level;//Level will consist of an array of informations about level like enemy density, wing formations and types, for automatic generation.
+                      //Will also contain informations in a form of array about backgrounds in levels existing in games. 
+                      //Each level will provide a chance to build up - equipement, lives and score, for a moment when you run out of level information.
+                      //Once end of the array is met, generator will go 
+                      //full jenkins and generate stuff more randomly in more ramping difficulties.
+                      //Level should have a midpoint at which game starts generating next level, to ensure smooth transition
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!initialized)
+            return;
+
+	}
+
+    private bool IsSwitchConditionMet()
+    {
+
+    }
+
+    private bool GenerateNextLevel()
+    {
+
+    }
+}
+
